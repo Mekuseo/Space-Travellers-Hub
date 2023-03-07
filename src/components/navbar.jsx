a6f2ad60278cafac16825cdee2553cb3340c436e
tobby8629 <sarhmullah15@gmail.com>
2023-03-07 12:16:55 -0800

    create navbar for mobile and desktop

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  const [menu, setMenu] = useState(false);
+  const changebar = () => { setMenu(!menu); };
   return (
-    <div className='navbar'>
-      <div className="logo">
-        <h1>Space X</h1>
-      </div>
-      <ul>
-        <li>
-          <Link to="/"> Rocket </Link>
-        </li>
-        <li>
-          <Link to="mission"> Mission </Link>
-        </li>
-        <li>
-          <Link to="profile"> profile </Link>
-        </li>
-      </ul>
-    </div>
+    <nav>
+       <div className="logo">
+          <Link to="/"> Space X</Link>
+        </div>
+
+        <div className="menubar" onClick={changebar} onKeyDown={changebar} role="none">
+          {menu ? <i className="fa-solid fa-x" /> : <i className="fa-solid fa-bars" /> }
+        </div>
+
+        <ul
+          onClick={() => setMenu(false)}
+          onKeyDown={() => setMenu(false)}
+          role="none"
+          className={menu ? 'show' : 'hide'}
+        >
+          <li>
+            <Link to="/"> Rocket </Link>
+          </li>
+          <li>
+            <Link to="mission"> Mission </Link>
+          </li>
+
+          <li>
+            <Link to="profile"> Profile </Link>
+          </li>
+        </ul>
+      </nav>
   )
 }
 
