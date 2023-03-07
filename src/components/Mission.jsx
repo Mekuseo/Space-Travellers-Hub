12236301417aeae85c20f050687b5f59ba8a6153
tobby8629 <sarhmullah15@gmail.com>
2023-03-07 13:37:32 -0800

    setup linter

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 function Mission() {
   return (
     <div>Mission</div>
-  )
+  );
 }
 
-export default Mission
+export default Mission;
--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 function Rockets() {
   return (
     <div>Rockets</div>
-  )
+  );
 }
 
-export default Rockets
+export default Rockets;
