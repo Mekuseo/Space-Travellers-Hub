14dd9c0b090d11c62c44002a51367e7c933b4285
Chukwuemeka Kanu <mekuseo@gmail.com>
2023-03-08 09:24:10 +0100

    Created Mission Slice and fetched data from api

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchmission } from '../redux/Mission/missionSlice';
 
 function Mission() {
+  const mission = useSelector((state) => state.Mission.mission);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchmission());
+  }, [dispatch]);
   return (
-    <div>
-      mission page
+    <div className="mii">
+      {mission.map((miss) => (
+        <div className="amission" key={miss.id}>
+          <h3>{miss.name}</h3>
+          <div className="text">
+            {miss.description}
+          </div>
+          <div className="status">
+            <p>{miss.member ? <span>active member</span> : <span> not a member </span> }</p>
+            <div className="btn">
+              {miss.member
+                ? <button type="button"> leave mission </button>
+                : <button type="button"> join mission </button>}
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
