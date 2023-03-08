f5f92cfcf2a5c74c2a89bcc6872579e35c1bf9c5
tobby8629 <sarhmullah15@gmail.com>
2023-03-08 03:28:33 -0800

    style rocket and profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { cancel } from '../redux/Rockets/RocketSlice';
 
 function Profile() {
   const rocket = useSelector((state) => state.Rocket.rockets);
   const reserved = rocket.filter((rock) => rock.reserve === true);
+  const leng = reserved.length;
+  const dispatch = useDispatch();
+  const handlecancel = (id) => {
+    dispatch(cancel(id));
+  };
   return (
-    <div className="proof">
-      <ul className="reserve">
-        {reserved.map((reserve) => (
-          <li key={reserve.id}>
-            {reserve.rocket_name}
-          </li>
-        ))}
-      </ul>
+    <div className="profile">
+      <h2>My Mission</h2>
+      <div className="proof">
+        {leng === 0 ? <p className="center">oh! no rocket reserved, please reserve a rocket</p> : (
+          <ul className="reserve">
+            {reserved.map((reserve) => (
+              <li key={reserve.id}>
+                <p>{reserve.rocket_name}</p>
+                <a href={reserve.link}> see more</a>
+                <button type="button" onClick={() => handlecancel(reserve.id)}>cancel reserve</button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
+
   );
 }
 
