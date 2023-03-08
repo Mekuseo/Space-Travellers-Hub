d3118f5059278488ea6bee59547661e58573909c
tobby8629 <sarhmullah15@gmail.com>
2023-03-08 03:19:32 -0800

    profile filtering

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
 function Mission() {
   return (
-    <div>
+    <div className="mii">
       mission page
     </div>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Profile() {
+  const rocket = useSelector((state) => state.Rocket.rockets);
+  const reserved = rocket.filter((rock) => rock.reserve === true);
   return (
-    <div>Profile</div>
+    <div className="proof">
+      <ul className="reserve">
+        {reserved.map((reserve) => (
+          <li key={reserve.id}>
+            {reserve.rocket_name}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
