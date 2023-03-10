06b6d63c2c2c085457740f0a90252f6df0e79c50
tobby8629 <sarhmullah15@gmail.com>
2023-03-10 08:48:24 -0800

    create test for profile and mission component

--- src/redux/Mission/missionSlice.js
+++ src/redux/Mission/missionSlice.js
@@ -1,9 +1,8 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
 
 export const fetchmission = createAsyncThunk('missions/fetchmission', async () => {
-  const response = await axios.get('https://api.spacexdata.com/v3/missions');
-  const { data } = response;
+  const response = await fetch('https://api.spacexdata.com/v3/missions');
+  const { data } = response.json();
   return data;
 });
 
@@ -33,34 +32,6 @@ const missionSlice = createSlice({
       const updatedMission = {
         ...state.missions[missionIndex],
         member: false,
-      };
-      const updatedMissions = [...state.missions];
-      updatedMissions[missionIndex] = updatedMission;
-      return {
-        ...state,
-        missions: updatedMissions,
-      };
-    },
-    reserveMission: (state, action) => {
-      const missionIndex = state.missions.findIndex((mission) => mission.id === action.payload);
-      const updatedMission = {
-        ...state.missions[missionIndex],
-        member: false,
-        reserved: true,
-      };
-      const updatedMissions = [...state.missions];
-      updatedMissions[missionIndex] = updatedMission;
-      return {
-        ...state,
-        missions: updatedMissions,
-      };
-    },
-    cancelReservation: (state, action) => {
-      const missionIndex = state.missions.findIndex((mission) => mission.id === action.payload);
-      const updatedMission = {
-        ...state.missions[missionIndex],
-        member: false,
-        reserved: false,
       };
       const updatedMissions = [...state.missions];
       updatedMissions[missionIndex] = updatedMission;
